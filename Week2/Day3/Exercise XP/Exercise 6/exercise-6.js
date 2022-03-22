9906144155f5a7a3fa5bad78053e93e6f2cf1a2b
idanb93 <idanbar93@gmail.com>
2022-03-22 16:16:43 +0200

    looping without using for in

--- Week2/Day3/Exercise XP/Exercise 6/exercise-6.js
+++ Week2/Day3/Exercise XP/Exercise 6/exercise-6.js
@@ -6,6 +6,12 @@ let details = {
 
 // console.log “my name is Rudolf the raindeer”
 
+// for (key in details){
+//   if (details.hasOwnProperty(key)){
+//     process.stdout.write(`${key} ${details[key]} `);
+//   }
+// }
+
 let detailsKeys = Object.keys(details);
 
 detailsKeys.forEach( (key)=>{
