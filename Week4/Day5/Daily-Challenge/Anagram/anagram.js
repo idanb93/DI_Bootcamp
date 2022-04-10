6d77df3ea3bb1020adb1f8c559a83b5ab497fa9c
idanb93 <idanbar93@gmail.com>
2022-04-10 10:27:08 +0300

    daily-challenge

--- Week4/Day5/Daily-Challenge/Anagram/anagram.js
+++ Week4/Day5/Daily-Challenge/Anagram/anagram.js
@@ -1,8 +1,47 @@
 
-function isAnagram(stringA, stringB){
+
+function createLettersMapper(stringToMap) {
+
+    let tempObject = {};
+
+    for (let letter of stringToMap) {
+        if (tempObject.hasOwnProperty(letter)) {
+            tempObject[letter]++;
+        } else {
+            tempObject[letter] = 1;
+        }
+    }
+
+    return tempObject;
+}
+
+function isAnagram(stringA, stringB) {
+
+    if (stringA.length !== stringB.length) {
+        return false;
+    }
+
+    let stringALettersMapper = createLettersMapper(stringA);
+    let stringBLettersMapper = createLettersMapper(stringB);
+
+    let stringALettersMapperKeys = Object.keys(stringALettersMapper);
+
+    stringALettersMapperKeys.forEach((key) => {
+
+        if (!stringBLettersMapper.hasOwnProperty(key) ||
+            stringBLettersMapper[key] !== stringALettersMapper[key]) {
+            return false;
+        }
+    })
+
+    return true;
 
 }
 
-let str2 = "       School master      ";
-let str1 = str2.trim();
-console.log(str1);
+let str1 = "       Moon starer  ";
+let str1Cleaned = str1.trim().replace(" ", "").toLowerCase();
+
+let str2 = "                             Astronomer"
+let str2Cleaned = str2.trim().replace(" ", "").toLowerCase();
+
+console.log(isAnagram(str1Cleaned, str2Cleaned));
