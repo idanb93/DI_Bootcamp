457f5228c6097fc7015735cc52a28080b2f93a64
idanb93 <idanbar93@gmail.com>
2022-04-11 13:26:01 +0300

    show-only-letters-completed

--- Week3/Day3/Daily-Challenge/Show-Only-The-Letters/js/show-only-letters.js
+++ Week3/Day3/Daily-Challenge/Show-Only-The-Letters/js/show-only-letters.js
@@ -1,34 +1,37 @@
 
-// event-handlres solution
-
 (function createPage() {
 
     let inputLabel = document.createElement('label');
     inputLabel.textContent = "Please enter some letters: ";
     document.body.appendChild(inputLabel);
-    
+
     let input = document.createElement('input');
     document.body.appendChild(input);
-    
-    input.addEventListener('keyup', (event)=>{
 
-        for (let i = 0; i < (input.value).length; i++ ){
-            if ( (input.value).charCodeAt(i) < 48 || (input.value).charCodeAt(i) > 90){
-                console.log((input.value).charCodeAt(i));
-            }
+    input.addEventListener('keyup', (event) => {
+
+        // let inputCharisInt = parseInt(input.value[input.value.length - 1]);
+        let ipnutCharIsLetter = input.value[input.value.length - 1].match(/[a-zA-Z]/i);
+
+        if (ipnutCharIsLetter === null){
+            let tempArr = input.value.split('');
+            tempArr.splice(tempArr.length-1, 1);
+            input.value = tempArr.join('');
+
         }
-    })
-    
+
+    });
+
     let submitButton = document.createElement('button');
     submitButton.setAttribute('type', 'submit');
     submitButton.textContent = 'Submit';
-    
+
     submitButton.style.marginLeft = '10px';
     submitButton.style.height = '20px';
     submitButton.style.width = '70px';
-    
+
     document.body.appendChild(submitButton);
-    
+
 })();
 
 // regex solution
@@ -36,5 +39,5 @@
 // input.onkeyup = keyup;
 
 // function keyup(){
-//     this.value = this.value.replace(/^[a-zA-Z]+$/,'');  
+//     this.value = this.value.replace(/^[a-zA-Z]+$/,'');
 // }
